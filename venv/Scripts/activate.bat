13a5bb5a95b30377a3b025140fa0030ca4368150
okemzr <slawomir.job@merit-automotive.com>
2021-06-18 14:38:06 +0200

    database init

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\GIT\Budget_Planner\venv
+set VIRTUAL_ENV=C:\GIT\Budget_planner\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
