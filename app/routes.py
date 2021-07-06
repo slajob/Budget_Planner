a15565eb99fe7af6833b05cf095ff996fd1b30c8
okemzr <slawomir.job@merit-automotive.com>
2021-07-06 13:30:04 +0200

    date returned

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,7 @@ from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -58,4 +59,6 @@ def user(username):
         {'author': user, 'body': 'Test post #1'},
         {'author': user, 'body': 'Test post #2'}
     ]
+    date = datetime.now()
+    return str(date)
     return render_template('user.html', user=user, posts=posts)
